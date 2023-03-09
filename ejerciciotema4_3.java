04d46e5ebf5c52f7fb7f274cf17dc4a7ab792f31
alvaros86 <114369683+alvaros86@users.noreply.github.com>
2023-03-08 22:33:02 -0300

    Add files via upload

---
+++ ejerciciotema4_3.java
@@ -0,0 +1,13 @@
+package com.sentenciasdecontrol;
+
+public class ejerciciotema4_3 {
+
+    public static void main(String[] args) {
+
+        for (int numerFor = 0; numerFor <= 3; numerFor = numerFor + 1) {
+            System.out.println(numerFor);
+
+        }
+
+    }
+}
